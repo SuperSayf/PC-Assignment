5f20a9f8f21c001f16abc37ab43e205b8d82322e
Super Sayf <sayfullah.jumoorty@live.com>
2023-05-11 22:00:04 +0200

    Fixes

--- scan.cpp
+++ scan.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 #include <vector>
+#include <ctime>
 
 using namespace std;
 
-void validatePrefixSum(const vector<int>& temp, const vector<int>& final) {
+void validatePrefixSum(const vector<int> &temp, const vector<int> &final)
+{
     int curr = 0;
 
-    for (const auto& t : temp) {
+    for (const auto &t : temp)
+    {
         curr += t;
 
-        if (final[&t - &temp[0]] != curr) {
+        if (final[&t - &temp[0]] != curr)
+        {
             printf("INVALID PREFIX SUM\n");
             exit(1);
         }
@@ -18,15 +22,19 @@ void validatePrefixSum(const vector<int>& temp, const vector<int>& final) {
     printf("VALID PREFIX SUM\n");
 }
 
-void checkIfPowerOfTwo(int n) {
+void checkIfPowerOfTwo(int n)
+{
     // Check if the size of the input array is a power of two
-    if (n == 0) {
+    if (n == 0)
+    {
         cout << "Not a power of two" << endl;
         exit(1);
     }
-    
-    while (n != 1) {
-        if (n % 2 != 0) {
+
+    while (n != 1)
+    {
+        if (n % 2 != 0)
+        {
             cout << "Not a power of two" << endl;
             exit(1);
         }
@@ -34,54 +42,73 @@ void checkIfPowerOfTwo(int n) {
     }
 }
 
-void blelloch_scan(vector<int>& nums) {
+void blelloch_scan(vector<int> &nums)
+{
     int n = nums.size();
 
     // Up-sweep (reduce) phase
-    for (int d = 1; d < n; d *= 2) {
-        for (int i = 0; i < n; i += 2*d) {
-            nums[i+2*d-1] += nums[i+d-1];
+    int last = 0;
+    for (int d = 1; d < n; d *= 2)
+    {
+        for (int i = 0; i < n; i += 2 * d)
+        {
+            nums[i + 2 * d - 1] += nums[i + d - 1];
         }
+        last = nums[n - 1];
     }
 
     // Set root to 0
-    nums[n-1] = 0;
+    nums[n - 1] = 0;
 
     // Down-sweep (scan) phase
-    for (int d = n/2; d > 0; d /= 2) {
-        for (int i = 0; i < n; i += 2*d) {
-            int t = nums[i+d-1];
-            nums[i+d-1] = nums[i+2*d-1];
-            nums[i+2*d-1] += t;
+    for (int d = n / 2; d > 0; d /= 2)
+    {
+        for (int i = 0; i < n; i += 2 * d)
+        {
+            int t = nums[i + d - 1];
+            nums[i + d - 1] = nums[i + 2 * d - 1];
+            nums[i + 2 * d - 1] += t;
         }
     }
+
+    // Increase the size of the array by 1 and set the last element to the last value of the up-sweep phase
+    nums.resize(n + 1);
+    nums[n] = last;
+
+    // Remove the first element of the array
+    nums.erase(nums.begin());
 }
 
-int main() {
+int main()
+{
     vector<int> nums = {2, 1, 4, 0, 3, 7, 6, 3};
     vector<int> temp = nums;
 
     // Check if the size of the input array is a power of two
     checkIfPowerOfTwo(nums.size());
 
+    // Start the timer
+    clock_t start = clock();
+
     // Calculate the scan of the input array
     blelloch_scan(nums);
 
-    // Calculate the prefix sum of the input array
-    vector<int> final(nums.size());
-    for (size_t i = 0; i < nums.size(); i++) {
-        final[i] = nums[i] + temp[i];
-    }
-
-    // Check if the prefix sum is correct
-    validatePrefixSum(temp, final);
+    // Stop the timer
+    clock_t end = clock();
 
     // Print the prefix sum
-    for (size_t i = 0; i < final.size(); i++) {
-        cout << final[i] << " ";
+    for (size_t i = 0; i < nums.size(); i++)
+    {
+        cout << nums[i] << " ";
     }
 
     cout << endl;
+
+    // Print the time taken
+    cout << "Time taken: " << (double)(end - start) / CLOCKS_PER_SEC << "s" << endl;
+
+    // Check if the prefix sum is correct
+    validatePrefixSum(temp, nums);
 
     return 0;
 }
